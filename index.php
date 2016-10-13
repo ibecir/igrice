f04ed4e3e47f8a63b2cbc73abe367a9fda3cfbc1
Enis <enis_gegic@hotmail.com>
2016-10-13 13:19:12 +0200

    page router

--- index.php
+++ index.php
@@ -1,6 +1,28 @@
-<?php
-include 'includes/header.php';
-include 'includes/footer.php';
+ <?php
+header('Access-Control-Allow-Headers: *');
 
+error_reporting(0);
+ini_set('display_errors', 0);
+
+session_start();
+header('Access-Control-Allow-Origin: *');
+
+
+
+
+$arr = explode ( "/", $_GET [send] );
+
+$page = $arr [0];
+$action = $arr [1];
+$id = $arr [2];
+
+
+switch ($page) {
+	case '' :
+		include_once 'views/index.php';
+		break;
+	default :
+		include_once 'views/index.php';
+		break;
+}
 ?>
-
---
+++ views/index.php
@@ -0,0 +1,4 @@
+<?php
+echo "sve je u dure";
+
+?>
