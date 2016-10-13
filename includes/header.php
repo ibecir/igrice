ec5af771b2faa886b85568c6737f6857c22cf30e
ibecir <isakovic.becir@hotmail.com>
2016-10-13 11:31:38 +0200

    Initial Commit

---
+++ includes/header.php
@@ -0,0 +1,123 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+<meta charset="utf-8">
+<title>Igrice</title>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+<!-- Loading Bootstrap -->
+<link href="core/css/bootstrap.css" rel="stylesheet">
+<link href="core/css/style.css" rel="stylesheet">
+<link href="core/css/color.css" rel="stylesheet">
+<link href="core/css/responsive.css" rel="stylesheet">
+
+<!-- Font Awesome -->
+<link href="core/css/font-awesome.css" rel="stylesheet">
+<link href="core/css/slick.css" rel="stylesheet" type="text/css">
+<link href="core/css/slick-theme.css" rel="stylesheet" type="text/css">
+
+<!-- <link rel="shortcut icon" href="images/favicon.ico"> -->
+
+</head>
+<body>
+
+	<div id="top-bar-style1" class="catgroy-main">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-9 col-sm-9 col-xs-12">
+					<div class="theme-logo">
+						<a href="index.php"><img src="core/images/logo.png" alt="" /></a>
+					</div>
+					<div class="search-form clearfix">
+						<form>
+
+							<div class="search-field pull-left">
+								<input type="text" placeholder="Search here">
+							</div>
+							<div class="search-btn pull-left">
+								<button>
+									<i class="fa fa-search"></i>
+								</button>
+							</div>
+						</form>
+					</div>
+				</div>
+				<div class="col-md-3 col-sm-3 col-xs-12 text-right">
+					<div class="top-bar-upload pad-top-0">
+						<a href="index.php" class="upload-video"><i class="fa fa-upload"></i>Upload</a>
+						<a href="index.php" class="user-btn"><i class="fa fa-user"></i></a>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<!-- /.item -->
+
+	<div id="header-style1">
+
+		<div class="wrapper">
+
+			<div class="container">
+
+				<nav role="navigation" class="navbar plain">
+					<div class="navbar-header">
+						<button data-target="#navbar-collapse-02" data-toggle="collapse"
+							class="navbar-toggle" type="button">
+							<i class="fa fa-bars"></i>
+						</button>
+
+					</div>
+
+					<div id="navbar-collapse-02" class="collapse navbar-collapse">
+						<ul class="nav navbar-nav navbar-left">
+							<li class="dropdown"><a href="#" class="dropdown-toggle"
+								data-toggle="dropdown"><i class="fa fa-home"></i> Home </a>
+								<ul class="dropdown-menu">
+									<li><a href="index.html">Home Style 1</a></li>
+									<li><a href="index2.html">Home Style 2</a></li>
+									<li><a href="index3.html">Home Style 3</a></li>
+									<li><a href="index4.html">Home Style 4</a></li>
+
+
+
+								</ul></li>
+
+							<li class="propClone"><a href="index.php"> <i class="fa fa-film"></i>
+									movies
+							</a></li>
+							<li class="propClone"><a href="index.php"><i
+									class="fa fa-futbol-o"></i> sports</a></li>
+							<li class="propClone"><a href="index.php"><i class="fa fa-music"></i>
+									music</a></li>
+							<li class="propClone"><a href="index.php"><i
+									class="fa fa-user"></i> channels</a></li>
+							<li class="dropdown"><a href="index.php" class="dropdown-toggle"
+								data-toggle="dropdown"><i class="fa fa-file-text-o"
+									aria-hidden="true"></i>Pages</a>
+								<ul class="dropdown-menu">
+									<li><a href="#">Blog Page</a></li>
+									<li><a href="#">Blog Detail Page</a></li>
+									<li><a href="#">Detail Page</a></li>
+									<li><a href="#">Detail With Sidebar</a></li>
+									<li><a href="#">Detail With Sidebar2</a></li>
+									<li><a href="#">Subcategory Page</a></li>
+									<li><a href="#">contact Us Page</a></li>
+									<li><a href="#">Mychannel Page</a></li>
+
+
+
+								</ul></li>
+
+						</ul>
+					</div>
+					<!-- /.navbar-collapse -->
+				</nav>
+
+			</div>
+			<!-- /.container -->
+
+		</div>
+		<!-- /.wrpaper -->
+	</div>
+	<!-- /.item -->
