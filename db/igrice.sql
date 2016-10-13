f8c392f7bd9d0bd3cf1a8710ff9e1617aea5c048
ibecir <isakovic.becir@hotmail.com>
2016-10-13 12:01:17 +0200

    Database Dump

---
+++ db/igrice.sql
@@ -0,0 +1,35 @@
+/*
+SQLyog Ultimate v11.11 (64 bit)
+MySQL - 5.7.15-0ubuntu0.16.04.1 : Database - igrice
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`igrice` /*!40100 DEFAULT CHARACTER SET latin1 */;
+
+USE `igrice`;
+
+/*Table structure for table `igrice` */
+
+DROP TABLE IF EXISTS `igrice`;
+
+CREATE TABLE `igrice` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `name` varchar(256) NOT NULL,
+  `game_url` varchar(256) NOT NULL,
+  `icon_url` varchar(256) NOT NULL,
+  `description` varchar(256) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
--- index.php
+++ index.php
@@ -1,8 +1,6 @@
 <?php
-
 include 'includes/header.php';
 include 'includes/footer.php';
 
-echo 'Becir Isakovic';
-
 ?>
+
