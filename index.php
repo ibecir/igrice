3ead3b4a8379832210ae8e40813dfb29447e511e
ibecir <isakovic.becir@hotmail.com>
2016-10-13 13:45:05 +0200

    Initial Setup

--- index.php
+++ index.php
@@ -1,6 +1,25 @@
-<?php
-include 'includes/header.php';
-include 'includes/footer.php';
+ <?php
+header('Access-Control-Allow-Headers: *');
 
+error_reporting(0);
+ini_set('display_errors', 0);
+
+session_start();
+header('Access-Control-Allow-Origin: *');
+
+$arr = explode ( "/", $_GET [send] );
+
+$page = $arr [0];
+$action = $arr [1];
+$id = $arr [2];
+
+
+switch ($page) {
+	case 'becko' :
+		include_once 'home/index.php';
+		break;
+	default :
+		include_once 'home/index.php';
+		break;
+}
 ?>
-
