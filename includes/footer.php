6b13d8de874e5ed5ee60eb026018b996e77856af
ibecir <isakovic.becir@hotmail.com>
2016-11-01 10:54:32 +0100

    DbDump

--- includes/footer.php
+++ includes/footer.php
@@ -125,28 +125,20 @@
 
 <!--Load JS here for greater good=============================-->
 
-	<script>
+<script>
 		$(document).ready(function(){	
 		 "use strict";
 				$('[data-toggle="tooltip"]').tooltip({placement: "left"});   
 		});
 
-		var keys = {};
-		window.addEventListener("keydown",
-		    function(e){
-		        keys[e.keyCode] = true;
-		        switch(e.keyCode){
-		            case 37: case 39: case 38:  case 40: // Arrow keys
-		            case 32: e.preventDefault(); break; // Space
-		            default: break; // do not block other keys
-		        }
-		    },
-		false);
-		window.addEventListener('keyup',
-		    function(e){
-		        keys[e.keyCode] = false;
-		    },
-		false);
+		function KeyCheck(e) {
+            var key = e.charCode ? e.charCode : e.keyCode ? e.keyCode : 0;
+            if (key == 35 || key == 36 || key == 37 || key == 39) {
+                e.preventDefault();
+                return false;
+            }
+            return true;
+        }
 	</script>
 
 
