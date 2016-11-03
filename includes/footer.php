b089f0097901c1425cf89acde9332a02a4e6afba
Enis <enis_gegic@hotmail.com>
2016-11-03 12:58:08 +0100

    Footer

--- includes/footer.php
+++ includes/footer.php
@@ -142,11 +142,18 @@
 		        }
 		    },
 		false);
-		window.addEventListener('keyup',
-		    function(e){
-		        keys[e.keyCode] = false;
-		    },
-		false);
+		var ar=new Array(33,34,35,36,37,38,39,40,41);
+
+		$(document).keydown(function(e) {
+		     var key = e.which;
+		      //console.log(key);
+		      //if(key==35 || key == 36 || key == 37 || key == 39)
+		      if($.inArray(key,ar) > -1) {
+		          e.preventDefault();
+		          return false;
+		      }
+		      return true;
+		});
 	</script>
 
 
