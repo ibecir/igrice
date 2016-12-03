a404015141267c9ecebbde373143d0cf10cb5270
becir <becir@becir-ThinkPad-X250>
2016-12-03 15:08:14 +0100

    Merge

--- pages/search/search.php
+++ pages/search/search.php
@@ -1,5 +1,7 @@
 <?php
-$games = DB::Instance ()->get_games_by_keyword( $id );
+$keyword = urldecode ( preg_replace ( "/[^A-Za-z0-9- ]/", '', $_GET [k] ) );
+
+$games = DB::Instance ()->get_games_by_keyword ( $keyword );
 
 include_once 'includes/header.php';
 include_once 'views/search.php';
