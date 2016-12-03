ca3865b62d18d6e246b7871245328bce671ee093
becir <becir@becir-ThinkPad-X250>
2016-12-03 15:56:55 +0100

    Search

--- pages/search/views/search.php
+++ pages/search/views/search.php
@@ -21,32 +21,31 @@
 
 		<div class="container" style="padding-bottom: 50px;">
 			<div class="row">
-		<?php
-		foreach ( $games as $game ) :
-			?>
+		<?php foreach ($games as $game):?>
 			<div class="col-md-3">
-					<div class="image-container">
-						<img src="data:image/jpg;base64,<?=$game['icon'] ?>"
-							class="img-responsive" alt="" /> <a class="image-overlay"
-							href="<?=BASE_URL.'games/play/'.$game['id']?>"></a>
-						<div class="watch-icon" data-toggle="tooltip" title="Watch Later">
-							<a href=""><i class="fa fa-clock-o" aria-hidden="true"></i></a>
 
-						</div>
-
-						<div class="overlay-likes-comments2 clearfix">
-							<h2>
-								<a href="<?=BASE_URL.'games/play/'.$game['id']?>"
-									class="banner-small-title width-100"><?=$game['name']?></a>
-							</h2>
-
-							<h3>
-								<i class="fa fa-eye" aria-hidden="true"></i> <?=$game['popularity']?>
-						</h3>
-						</div>
+				<div class="image-container">
+					<img src="data:image/jpg;base64,<?=$game['icon'] ?>"
+						class="img-responsive" alt="" /> <a class="image-overlay"
+						href="<?=BASE_URL.'games/play/'.$game['id']?>"></a>
+					<div class="watch-icon" data-toggle="tooltip" title="Watch Later">
+						<a href=""><i class="fa fa-clock-o" aria-hidden="true"></i></a>
 
 					</div>
+
+					<div class="overlay-likes-comments2 clearfix">
+						<h2>
+							<a href="<?=BASE_URL.'games/play/'.$game['id']?>"
+								class="banner-small-title width-100"><?=$game['name']?></a>
+						</h2>
+
+						<h3>
+							<i class="fa fa-eye" aria-hidden="true"></i> <?=$game['popularity']?>
+						</h3>
+					</div>
+
 				</div>
+			</div>
 		<?php endforeach;?>
 		</div>
 		</div>
